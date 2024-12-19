bf6a4c2d4a0e1bd843affe2872fb468109873184
reymon10 <reymonalexander@gmail.com>
2024-12-19 18:50:03 -0500

    Consul in app_authors_spring OK

--- app_authors_spring/src/main/java/com/uce/distribuida/rh/app_authors_spring/AppAuthorsSpringApplication.java
+++ app_authors_spring/src/main/java/com/uce/distribuida/rh/app_authors_spring/AppAuthorsSpringApplication.java
@@ -2,7 +2,9 @@ package com.uce.distribuida.rh.app_authors_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class AppAuthorsSpringApplication {
 
--- app_authors_spring/src/main/java/com/uce/distribuida/rh/app_authors_spring/AuthorsLifecycle.java
+++ app_authors_spring/src/main/java/com/uce/distribuida/rh/app_authors_spring/AuthorsLifecycle.java
@@ -1,70 +1,54 @@
 package com.uce.distribuida.rh.app_authors_spring;
 
 
-import io.vertx.ext.consul.ConsulClientOptions;
-import io.vertx.ext.consul.ServiceOptions;
-import io.vertx.mutiny.core.Vertx;
-import io.vertx.mutiny.ext.consul.ConsulClient;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.event.Observes;
-import jakarta.inject.Inject;
 
-import java.net.InetAddress;
-import java.util.UUID;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.net.InetAddress;
-import java.util.UUID;
-
 @Component
+
 public class AuthorsLifecycle {
 
-    @Value("${consul.host:127.0.0.1}")
-    private String consulIp;
-
-    @Value("${consul.port:8500}")
-    private Integer consultPort;
-
-    @Value("${server.port}")
-    private Integer appPort;
-
-    private String serviceId;
-
-    private final Vertx vertx;
-
-    public AuthorsLifecycle(Vertx vertx) {
-        this.vertx = vertx;
-    }
-
-    @PostConstruct
-    public void init() throws Exception {
-        System.out.println("Iniciando servicio");
-        ConsulClient client = ConsulClient.create(vertx,
-                new ConsulClientOptions()
-                        .setHost(consulIp)
-                        .setPort(consultPort));
-
-        serviceId = UUID.randomUUID().toString();
-        var ipAddress = InetAddress.getLocalHost();
-
-        client.registerServiceAndAwait(new ServiceOptions()
-                .setName("app-authors")
-                .setId(serviceId)
-                .setAddress(ipAddress.getHostAddress())
-                .setPort(appPort));
-    }
-
-    @PreDestroy
-    public void stop() throws Exception {
-        System.out.println("Deteniendo servicio...");
-        ConsulClient client = ConsulClient.create(vertx,
-                new ConsulClientOptions()
-                        .setHost(consulIp)
-                        .setPort(consultPort));
-
-        client.deregisterServiceAndAwait(serviceId);
-    }
+//    @Value("${consul.host}")
+//    private String consulIp;
+//
+//    @Value("${consul.port}")
+//    private Integer consultPort;
+//
+//
+//    private String serviceId;
+//
+//    private final Vertx vertx;
+//
+//    public AuthorsLifecycle(Vertx vertx) {
+//        this.vertx = vertx;
+//    }
+//
+//    @PostConstruct
+//    public void init() throws Exception {
+//        System.out.println("Iniciando servicio");
+//        ConsulClient client = ConsulClient.create(vertx,
+//                new ConsulClientOptions()
+//                        .setHost(consulIp)
+//                        .setPort(consultPort));
+//
+//        serviceId = UUID.randomUUID().toString();
+//        var ipAddress = InetAddress.getLocalHost();
+//
+//        client.registerServiceAndAwait(new ServiceOptions()
+//                .setName("app-authors")
+//                .setId(serviceId)
+//                .setAddress(ipAddress.getHostAddress())
+//                .setPort(appPort));
+//    }
+//
+//    @PreDestroy
+//    public void stop() throws Exception {
+//        System.out.println("Deteniendo servicio...");
+//        ConsulClient client = ConsulClient.create(vertx,
+//                new ConsulClientOptions()
+//                        .setHost(consulIp)
+//                        .setPort(consultPort));
+//
+//        client.deregisterServiceAndAwait(serviceId);
+//    }
 }
